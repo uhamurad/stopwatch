11f87fc48787a047c5e3ac9475bd2ad6962fbd7a
almasmurad <uhamurad@gmail.com>
2024-01-29 16:49:52 +0500

    fill BaseStopwatch

--- src/Stopwatch/BaseStopwatch.php
+++ src/Stopwatch/BaseStopwatch.php
@@ -8,29 +8,51 @@ final class BaseStopwatch implements StopwatchInterface
     /**
      * The start time of the process.
      *
-     * @var int
+     * @var float
      */
     private $startTimestamp = 0;
     /**
      * The end time of the process.
      *
-     * @var int
+     * @var float
      */
     private $endTimestamp = 0;
 
     public function start(): StopwatchInterface
     {
+        $this->startTimestamp = $this->getCurrentTimestamp();
         return $this;
     }
 
     public function stop(): StopwatchInterface
     {
+        $this->endTimestamp = $this->getCurrentTimestamp();
         return $this;
     }
 
     public function report(): StopwatchInterface
     {
+
+        $elapsed = $this->endTimestamp - $this->startTimestamp;
+
+        $startedStr = date('r', $this->startTimestamp);
+        $elapsedStr = number_format($elapsed, 3, '.', ' ');
+
+        $message = "Started at {$startedStr}\n";
+        $message .= str_repeat('‾', mb_strlen($message))."\n";
+        $message .= "All time | {$elapsedStr}s\n";
+
+        echo $message;
+
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getCurrentTimestamp()
+    {
+        return microtime(true);
     }
 
 
