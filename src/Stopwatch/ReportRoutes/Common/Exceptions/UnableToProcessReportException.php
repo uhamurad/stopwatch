2f74a5f0add5acf65c4a66e26af6306308f235a5
almasmurad <uhamurad@gmail.com>
2024-02-17 15:46:51 +0500

    Refined error handling by removing redundant properties in UnableToProcessReportException.

--- src/Stopwatch/ReportRoutes/Common/Exceptions/UnableToProcessReportException.php
+++ src/Stopwatch/ReportRoutes/Common/Exceptions/UnableToProcessReportException.php
@@ -9,28 +9,9 @@ use Almasmurad\Stopwatch\Stopwatch\ReportRoutes\Common\ReportRouteInterface;
 
 class UnableToProcessReportException extends StopwatchException
 {
-    /**
-     * @var ReportRouteInterface
-     */
-    private $route;
-    /**
-     * @var string
-     */
-    private $report;
     public function __construct(ReportRouteInterface $route, string $report, string $message = "", int $code = 0, \Throwable $previous = null)
     {
+        $message = sprintf('%s. Route: %s, Report: %s', $message, get_class($route), $report);
         parent::__construct($message, $code, $previous);
-        $this->route = $route;
-        $this->report = $report;
-    }
-
-    public function getRoute(): ReportRouteInterface
-    {
-        return $this->route;
-    }
-
-    public function getReport(): string
-    {
-        return $this->report;
     }
 }
