3f48a3f73193d76c6d7e41522ebe12c0813957e3
almasmurad <uhamurad@gmail.com>
2024-03-19 17:20:28 +0500

    Add rector.php configuration file and update .gitignore

---
+++ rector.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withImportNames(importShortClasses: false)
+    ->withPhpSets(php70: true);
