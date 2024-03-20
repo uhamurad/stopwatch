9973c103e8a86caecc08efd7eef48b2938f4d609
almasmurad <uhamurad@gmail.com>
2024-03-20 08:44:58 +0500

    Add PHP version to rector configuration

--- rector.dist.php
+++ rector.dist.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
+    ->withPhpVersion(PhpVersion::PHP_70)
     ->withImportNames(importShortClasses: false)
     ->withPhpSets(php70: true);
