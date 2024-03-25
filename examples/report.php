cb7183bd0e5159abc8c4d89a9b4fedc82fce4406
almasmurad <uhamurad@gmail.com>
2024-03-25 10:55:26 +0500

    Fix report example

--- examples/report.php
+++ examples/report.php
@@ -11,16 +11,13 @@ $html = file_get_contents('https://csszengarden.com/examples/index') ?: '';
 preg_match_all('/\bhttps?:\/\/[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|\/))/i', $html, $match);
 $urls = $match[0];
 
-// Finish measuring
-$stopwatch->finish();
-
-// Option 1. Print report to standard output
+// Option 1. Finish measuring and print report to standard output
 $stopwatch->report();
 
-// Option 2. Output report to local file
-$stopwatch->reportToFile(__DIR__ . '/report/report.txt');
+// Option 2. Finish measuring and output report to local file
+$stopwatch->reportToFile(__DIR__ . '/runtime/report.txt');
 
-// Option 3. Get report object and use its parameters
+// Option 3. Finish measuring and get report object and use its parameters
 $report = $stopwatch->getReport();
 $elapsed = $report->getAllTime()->getSeconds();
 printf('Wow it spent %f seconds', $elapsed);
