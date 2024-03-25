fcbf096972518713dfd0d92fc62f963d6018ba06
almasmurad <uhamurad@gmail.com>
2024-03-25 10:50:15 +0500

    Add examples about custom report renderer and sender classes

---
+++ examples/customReportRenderer.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+
+use Almasmurad\Stopwatch\Report\Common\ReportInterface;
+use Almasmurad\Stopwatch\Report\Renderer\Common\ReportRendererInterface;
+
+// Declare new renderer class
+class OneLineReportRenderer implements ReportRendererInterface
+{
+    public function render(ReportInterface $report): string
+    {
+        $start = $report->getStartEvent()->getDateTime()->format('H:i:s');
+        $duration = (int)$report->getAllTime()->getSeconds();
+
+        return "Stopwatch started at {$start} and ran for {$duration} full seconds";
+    }
+}
+
+// Create Stopwatch and start measuring
+$stopwatch = new \Almasmurad\Stopwatch\Stopwatch();
+
+// (measured code)
+$html = file_get_contents('https://csszengarden.com/examples/index') ?: '';
+preg_match_all('/\bhttps?:\/\/[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|\/))/i', $html, $match);
+$urls = $match[0];
+
+// Finish measuring, set new renderer and print report to standard output
+$stopwatch->setReportRenderer(new OneLineReportRenderer)->report();
---
+++ examples/customReportSender.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Almasmurad\Stopwatch\Report\Sender\Common\ReportSenderInterface;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Declare new sender class
+class SyslogReportSender implements ReportSenderInterface
+{
+    public function send(string $report)
+    {
+        openlog("stopwatch", LOG_PID, LOG_LOCAL0);
+        syslog(LOG_INFO, $report);
+        closelog();
+    }
+}
+
+// Create Stopwatch with new sender and start measuring
+$stopwatch = new \Almasmurad\Stopwatch\Stopwatch(new SyslogReportSender);
+
+// (measured code)
+$html = file_get_contents('https://csszengarden.com/examples/index') ?: '';
+preg_match_all('/\bhttps?:\/\/[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|\/))/i', $html, $match);
+$urls = $match[0];
+
+// Finish measuring and send report to the syslog
+$stopwatch->report();
+
+// Let's check the syslog
+system('less /var/log/syslog');
